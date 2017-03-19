ef9400fde4abf8d171addbfdc43da11bb1ecd0b0
Ruslan Kalakutskiy <kalakuts@amazon.com>
2017-03-19 13:28:10 +1100

    fixed to work with knife

--- hostname/metadata.rb
+++ hostname/metadata.rb
@@ -1,2 +1,4 @@
 name "hostname"
 version "0.0.1"
+maintainer "Ruslan K."
+maintainer_email 'admin@example.com'
--- timezone/metadata.rb
+++ timezone/metadata.rb
@@ -1,2 +1,4 @@
 name "timezone"
 version "0.0.1"
+maintainer "Ruslan K."
+maintainer_email 'admin@example.com'
