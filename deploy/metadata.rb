c879ee880b902c50507d72abc36ea93a33665723
Ruslan Kalakutskiy <kalakuts@amazon.com>
2017-04-23 13:27:34 +1000

    deploy cookook

---
+++ deploy/metadata.rb
@@ -0,0 +1,5 @@
+
+name "undeploy"
+version "0.0.1"
+maintainer "Ruslan K."
+maintainer_email 'admin@example.com'
---
+++ deploy/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: undeploy
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+# output all apps
+
+data_bag("aws_opsworks_app").each do |app|
+    log "Chef::JSONCompat.to_json_pretty(#{app})"
+end
