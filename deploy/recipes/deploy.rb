bccc792261d82a84c156121264a2fcc85d098e3d
Ruslan Kalakutskiy <kalakuts@amazon.com>
2017-04-24 16:28:52 +1000

    Fix deploy recipe

--- deploy/recipes/deploy.rb
+++ deploy/recipes/deploy.rb
@@ -13,6 +13,6 @@ end
 log "====> Example of using aws_opsworks_command data_bag"
 command = search("aws_opsworks_command", "type:deploy").first
 command["args"]["app_ids"].each do |my_app_id|
-    app = search("aws_opsworks_app", "app_id:#{my_app_id}")
+    app = search("aws_opsworks_app", "app_id:#{my_app_id}").first
     log "Application #{app['name']} should be deployed to #{app['attributes']['document_root']} on instance #{command['instance_id']}."
 end
